53ac328250a22a0abf8f889bf9cb872acca14c60
Torin Zhou <torinzhou@gmail.com>
2023-10-31 22:41:01 +0800

    working with api 1

---
+++ 03_API_Practice/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// API: Any protocol specifying how software components should interact with each other
+
+using var client = new HttpClient();
+// Uri: Uniform* Resource Identifier; do not omit the / at the end
+client.BaseAddress = new Uri("https://datausa.io/api/");
+
+// the return type response, is a Task<HttpResponseMessage>, it's a task, not the result itself, 
+// for now we have to add the await keyword.
+//var response = client.GetAsync("data?drilldowns=Nation&measures=Population");
+
+// now the response type is the response message itself as we need
+var response = await client.GetAsync("data?drilldowns=Nation&measures=Population");
+
+response.EnsureSuccessStatusCode();
+
+string json = await response.Content.ReadAsStringAsync();
+
+// the api: https://datausa.io/api/data?drilldowns=Nation&measures=Population
+
+
+
+
+
+
+
+
+Console.ReadKey();
