566a2978ef35b2516214aa87588f99664765d5d0
Torin Zhou <torinzhou@gmail.com>
2023-11-15 13:26:37 +0800

    LinkedList Assignment First Draft

--- 19_Assignment_CustomLinkedList_Implementation/Program.cs
+++ 19_Assignment_CustomLinkedList_Implementation/Program.cs
@@ -1,17 +1,52 @@
-﻿
+﻿using System.Collections;
 
-using System.Collections;
+var testLinkedList = new CustomLinkedList<int>(1, 3, 5, 7, 8);
+testLinkedList.Add(1);
+foreach (var item in testLinkedList)
+{
+    Console.WriteLine(item);
+}
+
+Console.WriteLine($"The Count is: {testLinkedList.Count}");
+Console.WriteLine($"Contains 7: {testLinkedList.Contains(7)} ");
+
+var testArray = new int[99];
+testLinkedList.CopyTo(testArray, 3);
+Console.WriteLine($"{testLinkedList.Remove(3)}");
 
 Console.ReadKey();
+
+
 
 public interface ILinkedList<T> : ICollection<T>
 {
     void AddToFront(T? item);
     void AddToEnd(T? item);
 }
-public class CustomLikedList<T> : ILinkedList<T>
+public class CustomLinkedList<T> : ILinkedList<T>
 {
-    public int Count => throw new NotImplementedException();
+    public Node<T>? Head { get; set; }
+    public Node<T>? Tail { get; set; }
+    public int Count { get; private set; }
+
+    public CustomLinkedList(params T[]? input)
+    {
+        if (input is null)
+        {
+            Head = null;
+        }
+        Head = new Node<T>(input[0]);
+        var current = Head;
+        Count = input.Count();
+        for (var i = 1; i < input.Length; i++)
+        {
+            var newNode = new Node<T>(input[i]);
+            current.Next = newNode;
+            current = newNode;
+            Tail = current;
+        }
+    }
+
 
     public bool IsReadOnly => false;
 
@@ -22,7 +57,10 @@ public class CustomLikedList<T> : ILinkedList<T>
 
     public void AddToEnd(T? item)
     {
-        throw new NotImplementedException();
+        var node = new Node<T>(item);
+        Tail.Next = node;
+        Tail = node;
+        Count++;
     }
 
     public void AddToFront(T? item)
@@ -37,24 +75,72 @@ public class CustomLikedList<T> : ILinkedList<T>
 
     public bool Contains(T? item)
     {
-        throw new NotImplementedException();
+        var current = Head;
+        while (current is not null)
+        {
+            if (current.Value.Equals(item))
+            {
+                return true;
+            }
+            current = current.Next;
+        }
+        return false;
     }
 
     public void CopyTo(T[] array, int arrayIndex)
     {
-        throw new NotImplementedException();
+        if (array is null)
+        {
+            throw new NullReferenceException();
+        }
+        var current = Head;
+        int index = arrayIndex;
+        while (current is not null)
+        {
+            array[index] = current.Value;
+            current = current.Next;
+            index++;
+        }
     }
 
-
-    
     public bool Remove(T? item)
-        //todo remove the first node whose data is equal to the given argument, including null
+    //todo remove the first node whose data is equal to the given argument, including null
     {
-        throw new NotImplementedException();
+        if (Head is null)
+        {
+            return false;
+        }
+        if (Head.Value.Equals(item))
+        {
+            Head = Head.Next;
+            if (Head == null) Tail = null;
+            Count--;
+            return true;
+        }
+
+        var current = Head;
+        while(current.Next is not null)
+        {
+            if(current.Next.Value.Equals(item))
+            {
+                current.Next = current.Next.Next;
+                if(current.Next == null) Tail = current;
+                Count--;
+                return true;
+            }
+            current = current.Next;
+        }
+
+        return false;
     }
     public IEnumerator<T> GetEnumerator()
     {
-        throw new NotImplementedException();
+        Node<T>? current = Head;
+        while (current is not null)
+        {
+            yield return current.Value;
+            current = current.Next;
+        }
     }
 
     IEnumerator IEnumerable.GetEnumerator()
@@ -65,6 +151,12 @@ public class CustomLikedList<T> : ILinkedList<T>
 
 public record Node<T>
 {
-    T? Value { get; set; }
-    Node<T>? Next { get; set; }
+    public T? Value { get; init; }
+    public Node<T>? Next { get; set; }
+
+    public Node(T? value)
+    {
+        Value = value;
+    }
+
 }
