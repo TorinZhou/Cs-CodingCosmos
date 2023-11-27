13e40aa10935465517e06636da3550d699dad175
Torin Zhou <torinzhou@gmail.com>
2023-11-27 23:17:06 +0800

    Unit Test Assignment, before implementing testing

---
+++ 32_DiceRollGame/Program.cs
@@ -0,0 +1,18 @@
+﻿using DiceRollGame.Game;
+using UserCommunication;
+
+var random = new Random();
+var dice = new Dice(random);
+var consoleUserCommunication = new ConsoleUserCommunication();
+var guessingGame = new GuessingGame(dice, consoleUserCommunication);
+
+GameResult gameResult = guessingGame.Play();
+guessingGame.PrintResult(gameResult);
+
+Console.ReadKey();
+
+
+
+
+
+
---
+++ 32_DiceRollGameTests/GuessingGameTests.cs
@@ -0,0 +1,21 @@
+﻿using DiceRollGame.Game;
+using Moq;
+using NUnit.Framework;
+
+namespace _32_DiceRollGameTests
+{
+    [TestFixture]
+    public class GuessingGameTests
+    {
+        [Test]
+        public void Play_ShallReturnVictory_IfTheUserGuessesTheNumberOnTheFirstTry()
+        {
+            var diceMock = new Mock<IDice>();
+            const int NumberOndie = 3;
+            diceMock.Setup(mock => mock.Roll()).Returns(NumberOndie);
+            var cut = new GuessingGame(diceMock.Object);
+            
+            var gameResult = cut.Play();
+        }
+    }
+}
