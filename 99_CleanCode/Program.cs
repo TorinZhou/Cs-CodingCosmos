82858522283c51688d5f78754d8ece9fa9812757
Torin Zhou <torinzhou@gmail.com>
2023-12-02 11:28:55 -0500

    switch to VS code on mac

---
+++ 99_CleanCode/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace _99_CleanCode;
+
+public class DataAccess : IDataAccess
+{
+    public List<int> GetData()
+    {
+        //imagine it connect to a DB and reads the data
+        return new List<int> { 1, 2, 3 };
+
+    }
+}
+
+public class DataProcessor
+{
+    private readonly IDataAccess _dataAccess;
+
+    public DataProcessor(IDataAccess dataAccess)
+    {
+        _dataAccess = dataAccess;
+    }
+
+    public int CalculateSum()
+    {
+        return _dataAccess.GetData().Sum();
+    }
+}
+
+public interface IDataAccess
+{
+    List<int> GetData();
+}
