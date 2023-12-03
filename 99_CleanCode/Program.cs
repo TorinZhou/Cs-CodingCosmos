33f9079477a8cee48d2482842f959d6a542bff97
Torin Zhou <torinzhou@gmail.com>
2023-12-03 00:25:08 -0500

    Composition vs Inheritance 2

--- 99_CleanCode/Program.cs
+++ 99_CleanCode/Program.cs
@@ -1,10 +1,13 @@
 ﻿using static System.Console;
-bool shallReadFromDatabase = true;
-var personalDataFormatter = new PersonDataFormatter();
+bool shallReadFromDatabase = false;
+PersonDataFormatter personalDataFormatter = (shallReadFromDatabase) ?
+    new DatabaseSourcePersonalDataFormatter() :
+    new ExcelSourcePersonalDataFormatter();
 WriteLine(personalDataFormatter.Format());
 
 ReadKey();
-class PersonDataFormatter
+record struct Person(string Name, int YearOfBirth, string Country);
+abstract class PersonDataFormatter
 {
     public string Format()
     {
@@ -14,16 +17,37 @@ class PersonDataFormatter
             $" {people.Country} on {people.YearOfBirth}"));
     }
 
-    public IEnumerable<Person> ReadPeople()
+    public abstract IEnumerable<Person> ReadPeople();
+}
+
+class DatabaseSourcePersonalDataFormatter : PersonDataFormatter
+{
+
+    public override IEnumerable<Person> ReadPeople()
     {
         WriteLine("Reading from database");
         return new List<Person>
         {
-            new Person("John", 1982, "USA"),
-            new Person("Aja", 1992, "India"),
-            new Person("Tom", 1954, "Australia"),
+            new Person("Jonh", 1982, "USA"),
+            new Person("Aja", 1982, "India"),
+            new Person("Tom", 1982, "Australia"),
         };
     }
 }
 
-record struct Person(string Name, int YearOfBirth, string Country);
+class ExcelSourcePersonalDataFormatter : PersonDataFormatter
+{
+
+    public override IEnumerable<Person> ReadPeople()
+    {
+        WriteLine("Reading from Excel File");
+        return new List<Person>
+        {
+            new Person("Martin", 1972, "France"),
+            new Person("Aiko", 1982, "Japan"),
+            new Person("Selene", 1982, "UK"),
+        };
+    }
+}
+
+// first I made the class abstract, then made the ReadPeople abstract by deleting the body
