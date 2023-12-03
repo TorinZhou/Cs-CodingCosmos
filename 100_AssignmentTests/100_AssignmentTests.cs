f4fe029328cea29cbdf2daf22923320e8c8e5be8
Torin Zhou <torinzhou@gmail.com>
2023-12-03 09:31:38 -0500

    try testing in vscode

---
+++ 100_AssignmentTests/100_AssignmentTests.cs
@@ -0,0 +1,16 @@
+using _100_Assignment;
+namespace _100_AssignmentTests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
